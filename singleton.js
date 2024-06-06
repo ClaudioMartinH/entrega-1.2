54ba3843c3c717e485f9255b62c57044e376c6e8
Claudio Martin <klaudito46@gmail.com>
2024-06-06 11:34:01 +0200

    quitado comentarios

--- singleton.js
+++ singleton.js
@@ -1,15 +1,8 @@
-//Ejemplo por Fernando Herrera ---> https://www.youtube.com/watch?v=rrWRhrdwuLg
-
-
 class Singleton {
-
-    static instancia;  //por defecto es undefined al declarar la variable vacía
-    nombre = "";       //hacemos una propiedad global que es "nombre"
+    nombre = "";       
 
     constructor( nombre = "") {
-        //comprobamos si la instancia ya existe, si no existe la creamos y devolvemos la instancia creada
         if (!!Singleton.instancia) {
-          //la doble negación transforma los valores en su equivalente booleano
           return Singleton.instancia;
         }
 
@@ -18,12 +11,11 @@ class Singleton {
     }
 
 }
-//creamos 3 nuevas instancias para comprobar que el output en los 3 casos será el mismo ya que los 3 se refieren al mismo objeto y al mismo vector de memoria
 const instancia1 = new Singleton("IronMan");
 const instancia2 = new Singleton("SpiderMan");
 const instancia3 = new Singleton("Black Panther");
 
-console.log(`Nombre en la instancia1 es: ${instancia1.nombre}`); //output: "Nombre en la instancia1 es: IronMan"
-console.log(`Nombre en la instancia2 es: ${instancia2.nombre}`); //output: "Nombre en la instancia2 es: IronMan"
-console.log(`Nombre en la instancia3 es: ${instancia3.nombre}`); //output: "Nombre en la instancia3 es: IronMan"
+console.log(`Nombre en la instancia1 es: ${instancia1.nombre}`);
+console.log(`Nombre en la instancia2 es: ${instancia2.nombre}`); 
+console.log(`Nombre en la instancia3 es: ${instancia3.nombre}`); 
 
